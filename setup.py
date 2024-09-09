72a5f513f781a7ef0a52b1097ee57ca287c9392b
Rayini Arun <arunrayini36@gmail.com>
2024-09-08 23:50:30 -0400

    updated files

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fbi_wanted_list',
-    version='1.0.0',
+    name='assignment0',
+    version='1.0',
     author='Arun Kumar Reddy Rayini',
     author_email='rayini.a@ufl.edu',
     packages=find_packages(exclude=('tests', 'docs')),
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    description='A package for fetching and formatting FBI wanted data',
+    tests_require=['pytest']
 )
